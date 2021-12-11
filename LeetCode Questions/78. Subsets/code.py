e44aabac2feb33251353ee44ac2ddffeee23df4e
Meinan <71438233+KatieGou@users.noreply.github.com>
2021-12-11 21:30:56 +0100

    Update code.py

--- LeetCode Questions/78. Subsets/code.py
+++ LeetCode Questions/78. Subsets/code.py
@@ -2,7 +2,10 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         def dfs(arr, subset, res):
             res.append(subset)
+            print(arr, subset, res)
+            print()
             for i in range(len(arr)):
+                print('arr=', arr, 'i=', i, 'arr[i]=', arr[i])
                 dfs(arr[i+1:], subset+[arr[i]], res)
         res=list()
         dfs(nums, [], res)
